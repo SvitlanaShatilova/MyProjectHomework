036040492a02fa5437e48ca6806fe9ca1dab882e
SvitlanaShatilova <svit.lana.shati@gmail.com>
2023-06-05 22:10:58 +0300

    homework

--- src/Homework0306/Zoo.java
+++ src/Homework0306/Zoo.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 /*Використовуючи Intelij IDEA, створити проект, пакет. Створити клас Zoo.
 У класі створити список, який записати 8 тварин через метод add(index, element). Вивести список у консоль.*/
+
 public class Zoo {
     public static void main(String[] args) {
         ArrayList<String> zoo = new ArrayList<>();
@@ -19,6 +20,15 @@ public class Zoo {
         System.out.println(zoo);
         for (String i : zoo){
             System.out.println(i);
+
+/*Використовуючи клас Zoo Завдання 2, видалити 3, 5, 7 тварин, дізнатися розмір списку і вивести в консоль.*/
+        zoo.remove(7);
+        zoo.remove(5);
+        zoo.remove(3);
+
+            System.out.println(zoo.size());
+            System.out.println(zoo);
+
         }
     }
 }
