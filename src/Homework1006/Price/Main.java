d3e7eb4c39b76a3b8d0fb217a7e42d568b0cc779
SvitlanaShatilova <svit.lana.shati@gmail.com>
2023-06-18 14:19:36 +0300

    homework

---
+++ src/Homework1006/Price/Main.java
@@ -0,0 +1,46 @@
+package Homework1006.Price;
+
+import Homework1006.Worker.UserException;
+
+import java.util.ArrayList;
+
+/*Описати клас з іменем Price, що містить такі поля:
+  назву товару;
+  назву магазину, в якому продається товар;
+  вартість товару у гривнях.
+ Написати програму, яка виконує такі дії:
+  введення з клавіатури даних до масиву, що складається з двох елементів типу Price (записи мають бути
+ впорядковані в алфавітному порядку за назвами магазинів);
+  виведення на екран інформації про товари, що продаються в магазині,
+  назва якого введена з клавіатури (якщо такого магазину немає, вивести виняток).*/
+public class Main {
+    public static void main(String[] args) throws UserException {
+
+        Price price1 = new Price("Малятко", "Абетка", 120.00);
+        Price price2 = new Price("Антошка", "Коляска", 12500.00);
+
+        ArrayList<Price> pricesList = new ArrayList<>();
+        pricesList = (ArrayList<Price>) Price.addListShop(price1, price2);
+
+
+        String name = "Антошка";
+       // Price.serchName(pricesList, name); //не працює, чому?
+
+      try {
+          for (int i = 0; i <= pricesList.size(); i++) {
+              int n=0;
+              if (pricesList.get(i).nameShop.equals(name)) {
+                  System.out.println("* " + pricesList.get(i));
+                  n+=1;
+                  System.out.println("співпадінь з введеною назвою = " + n);
+              }
+              if (n==0){
+                  throw new UserException("Такого магазину не існує"); // ЧОМУ ЦЯ ПОМИЛКА СПРАЦБОВУЄ ???
+              }
+          }
+      }catch (UserException e){
+          System.out.println(e);
+      }
+    }
+}
+
---
+++ src/Homework1006/Price/Price.java
@@ -0,0 +1,56 @@
+package Homework1006.Price;
+
+import Homework1006.Worker.UserException;
+
+import java.util.*;
+
+/*Описати клас з іменем Price, що містить такі поля:
+  назву товару;
+  назву магазину, в якому продається товар;
+  вартість товару у гривнях.
+ Написати програму, яка виконує такі дії:
+  введення з клавіатури даних до масиву, що складається з двох елементів типу Price (записи мають бути
+ впорядковані в алфавітному порядку за назвами магазинів);
+  виведення на екран інформації про товари, що продаються в магазині,
+  назва якого введена з клавіатури (якщо такого магазину немає, вивести виняток).*/
+public class Price {
+
+    protected String nameShop;
+    protected String nameProduct;
+    protected double priceProduct;
+
+    public Price(String nameShop, String nameProduct, double priceProduct) {
+        this.nameShop = nameShop;
+        this.nameProduct = nameProduct;
+        this.priceProduct = priceProduct;
+    }
+
+    public static List<Price> addListShop(Price o1, Price o2) {
+        ArrayList<Price> listShop = new ArrayList<>(2);
+        listShop.add(0, o1);
+        listShop.add(1, o2);
+        Collections.sort(listShop, new Comparator<Price>() {
+            @Override
+            public int compare(Price o1, Price o2) {
+                return o1.nameShop.compareTo(o2.nameShop);
+            }
+        });
+        return listShop;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Магазин <<" + nameShop + ">> \nназва товару: " + nameProduct + " - ціна: " + priceProduct + "\n";
+    }
+
+    public static void serchName(ArrayList<Price> list, String name) throws UserException {
+        for (int i = 0; i > list.size(); i++) {
+            if (list.get(i).nameShop.equals(name)) {
+                System.out.println("* " + list.get(i));
+            }else {
+                throw new UserException("Такого магазину не існує");
+            }
+        }
+    }
+}
