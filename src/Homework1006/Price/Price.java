846d458844f85746aa6d8e41f595740a98623b44
SvitlanaShatilova <129193331+SvitlanaShatilova@users.noreply.github.com>
2023-06-23 20:19:27 +0300

    Update Price.java

--- src/Homework1006/Price/Price.java
+++ src/Homework1006/Price/Price.java
@@ -45,7 +45,7 @@ public class Price {
     public static void serchName(ArrayList<Price> list, String name)  {
         ArrayList <Price> temp = new ArrayList<>();
         temp = list;
-        for (int i = 0; i > temp.size(); i++) {
+        for (int i = 0; i < temp.size(); i++) {
             if (temp.get(i).nameShop.equals(name)) {
                 System.out.println("* " + temp.get(i));
 
